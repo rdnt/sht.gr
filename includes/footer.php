1f89ceb4dbd22b01f521a729b06a3b353fabfd65
ShtHappens796 <shithappens796@gmail.com>
2017-11-18 00:01:41 +0200

    Initial loader implementation

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,4 @@
 <!--  Scripts-->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="/js/materialize.min.js?v=<?=$version?>"></script>
 <script src="/js/init.js?v=<?=$version?>"></script>
