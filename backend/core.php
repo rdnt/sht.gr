86a710f910dc758ca614c0cac333c35d12ebaaac
ShtHappens796 <shithappens796@gmail.com>
2017-11-17 21:07:14 +0200

    Initial file and folder structure

---
+++ backend/core.php
@@ -0,0 +1,13 @@
+<?php
+// Error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// Used to reset the cache of certain files
+$version = "0.0.0";
+// Returns formatted page title
+function page_title($title) {
+    return "SHT <> $title";
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include $_SERVER['DOCUMENT_ROOT']."/backend/core.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include $_SERVER['DOCUMENT_ROOT']."/includes/head.php"; ?>
+</head>
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT']."/includes/nav.php"; ?>
+<main>
+<?php include $_SERVER['DOCUMENT_ROOT']."/includes/page/home.php"; ?>
+</main>
+<?php include $_SERVER['DOCUMENT_ROOT']."/includes/footer.php"; ?>
+</body>
+</html>
