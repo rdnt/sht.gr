0da781beb7e493706445db49808aa665511e64c7
ShtHappens796 <shithappens796@gmail.com>
2017-11-17 21:07:14 +0200

    Initial file and folder structure

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<!--  Scripts-->
+<script src="/js/materialize.min.js?v=<?=$version?>"></script>
+<script src="/js/init.js?v=<?=$version?>"></script>
---
+++ includes/head.php
@@ -0,0 +1,18 @@
+<!-- Metadata -->
+<title><?=page_title("Home")?></title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<!-- Favicons -->
+<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+<link rel="manifest" href="/images/favicon/manifest.json">
+<link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#0a0b0c">
+<link rel="shortcut icon" href="/images/favicon/favicon.ico">
+<meta name="msapplication-config" content="/images/favicon/browserconfig.xml">
+<meta name="theme-color" content="#0a0b0c">
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!-- Stylesheets -->
+<link href="/css/materialize.min.css?v=<?=$version?>" type="text/css" rel="stylesheet" media="screen"/>
+<link href="/css/style.css?v=<?=$version?>" type="text/css" rel="stylesheet" media="screen"/>
