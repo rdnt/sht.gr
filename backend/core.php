fcabb90faaf76a8cbc964450bc787b04a4de3440
ShtHappens796 <shithappens796@gmail.com>
2017-11-19 00:04:58 +0200

    Add preloader toggle variable

--- backend/core.php
+++ backend/core.php
@@ -5,6 +5,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 // Used to reset the cache of certain files
 $version = "0.0.0";
+// Toggle preloader
+$preloader = 0;
 // Returns formatted page title
 function page_title($title) {
     return "SHT <> $title";
--- includes/page/home.php
+++ includes/page/home.php
@@ -1,8 +1,1 @@
-<div id="loader">
-    <div id="right-panel" class="init-right"></div>
-    <div id="left-panel" class="init-left"></div>
-    <div id="logo-wrapper">
-        <div class="right-triangle"></div>
-        <div class="left-triangle"></div>
-    </div>
-</div>
+<?php include_once $_SERVER['DOCUMENT_ROOT']."/includes/components/preloader.php"; ?>
