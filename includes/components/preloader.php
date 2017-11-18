44e6b06d19885b591475da9384ac18a8db3cb495
ShtHappens796 <shithappens796@gmail.com>
2017-11-19 00:04:58 +0200

    Add preloader toggle variable

---
+++ includes/components/preloader.php
@@ -0,0 +1,10 @@
+<?php if ($preloader): ?>
+<div id="loader">
+    <div id="right-panel" class="init-right"></div>
+    <div id="left-panel" class="init-left"></div>
+    <div id="logo-wrapper">
+        <div class="right-triangle"></div>
+        <div class="left-triangle"></div>
+    </div>
+</div>
+<?php endif; ?>
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
-<?php include $_SERVER['DOCUMENT_ROOT']."/backend/core.php"; ?>
+<?php include_once $_SERVER['DOCUMENT_ROOT']."/backend/core.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php include $_SERVER['DOCUMENT_ROOT']."/includes/head.php"; ?>
+<?php include_once $_SERVER['DOCUMENT_ROOT']."/includes/head.php"; ?>
 </head>
 <body>
-<?php include $_SERVER['DOCUMENT_ROOT']."/includes/nav.php"; ?>
+<?php include_once $_SERVER['DOCUMENT_ROOT']."/includes/nav.php"; ?>
 <main>
-<?php include $_SERVER['DOCUMENT_ROOT']."/includes/page/home.php"; ?>
+<?php include_once $_SERVER['DOCUMENT_ROOT']."/includes/page/home.php"; ?>
 </main>
-<?php include $_SERVER['DOCUMENT_ROOT']."/includes/footer.php"; ?>
+<?php include_once $_SERVER['DOCUMENT_ROOT']."/includes/footer.php"; ?>
 </body>
 </html>
