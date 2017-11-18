5c215fbd3af92fe5e28bc81d89f2e41f025cbf3d
ShtHappens796 <shithappens796@gmail.com>
2017-11-18 04:21:47 +0200

    Preloader improvements

--- includes/page/home.php
+++ includes/page/home.php
@@ -1,8 +1,10 @@
 <div id="loader">
-    <div class="wrapper">
+    <div id="right-panel" class="init-right"></div>
+    <div id="left-panel" class="init-left"></div>
+    <div id="logo-wrapper">
         <div class="right-triangle"></div>
         <div class="left-triangle"></div>
     </div>
 </div>
 
-<img alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Big_Bear_Valley%2C_California.jpg/1200px-Big_Bear_Valley%2C_California.jpg">
+<img style="width: 100%;height: 99.5vh;" alt="" src="http://static3.businessinsider.com/image/529c795c6bb3f7b44f3706ed/big-data-will-drive-the-next-phase-of-innovation-in-mobile-computing.jpg">
