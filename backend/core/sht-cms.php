53491afeebcbf618562157395b4f6b1b42bb81cc
ShtHappens796 <shithappens796@gmail.com>
2017-11-20 05:52:51 +0200

    Session initialization and core changes

--- backend/core/sht-cms.php
+++ backend/core/sht-cms.php
@@ -1,15 +1,26 @@
 <?php
-// Error reporting
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-// Used to reset the cache of certain files
+// Used to reset the files' cache
 $version = "0.0.0";
-// Toggle preloader
+// Disable preloader for all users
 $preloader = 1;
-// Returns formatted page title
+// Enable error reporting
+$errors = 1;
+
 function page_title($title) {
+    // Returns formatted page title
     return "SHT <> $title";
+}
+
+if ($errors) {
+    // Error reporting
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
+if (session_status() == PHP_SESSION_NONE) {
+    // Start the session if it wasn't already started
+    session_start();
 }
 
 ?>
