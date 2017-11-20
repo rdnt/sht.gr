e9c94fb4dcad162ced6e6e5cdf4e5a109f90c875
ShtHappens796 <shithappens796@gmail.com>
2017-11-20 06:15:19 +0200

    Move to normal jquery

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -1,4 +1,4 @@
 <!--  Scripts-->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="/js/materialize.min.js?v=<?=$version?>"></script>
 <script src="/js/init.js?v=<?=$version?>"></script>
