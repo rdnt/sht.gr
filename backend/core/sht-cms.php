889636f45de58d9b0c79095bbaab8755e4248f72
ShtHappens796 <shithappens796@gmail.com>
2017-11-24 01:13:55 +0200

    HTTP/2 Server Push function

--- backend/core/sht-cms.php
+++ backend/core/sht-cms.php
@@ -1,10 +1,12 @@
 <?php
 // Used to reset the files' cache
-$version = "0.1.0";
+$version = "0.1.1";
 // Disable preloader for all users
 $preloader = 1;
 // Enable error reporting
 $errors = 1;
+// Default timezone to Europe/Athens
+date_default_timezone_set("Europe/Athens");
 
 function page_title($title) {
     // Returns formatted page title
@@ -21,6 +23,23 @@ if ($errors) {
 if (session_status() == PHP_SESSION_NONE) {
     // Start the session if it wasn't already started
     session_start();
+}
+
+function push_assets($version) {
+    $assets = array(
+        "/css/style.css?v=$version" => "style",
+        "/js/init.js?v=$version" => "script",
+        "/css/materialize.min.css?v=$version" => "style",
+        "/js/materialize.min.js?v=$version" => "script"
+    );
+
+    $string = null;
+    $counter = count($assets);
+    foreach ($assets as $asset => $as) {
+        $counter--;
+        $string = "Link: <$asset>; rel=preload; as=$as";
+        header($string, false);
+    }
 }
 
 ?>
