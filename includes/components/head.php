8b6f1ce5be8cef7c5e6a8113e9076adf8762af5f
ShtHappens796 <shithappens796@gmail.com>
2017-11-24 04:03:40 +0200

    Optimization changes

--- includes/components/head.php
+++ includes/components/head.php
@@ -1,7 +1,5 @@
-<!-- Metadata -->
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-<!-- Favicons -->
 <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
@@ -10,8 +8,10 @@
 <link rel="shortcut icon" href="/images/favicon/favicon.ico">
 <meta name="msapplication-config" content="/images/favicon/browserconfig.xml">
 <meta name="theme-color" content="#0a0b0c">
-<!-- Fonts -->
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<!-- Stylesheets -->
+<?php if ($preloader == 1): ?>
 <link href="/css/materialize.min.css?v=<?=$version?>" type="text/css" rel="stylesheet" media="screen"/>
 <link href="/css/style.css?v=<?=$version?>" type="text/css" rel="stylesheet" media="screen"/>
+<?php else: ?>
+<link href="/css/materialize.min.css?v=<?=$version?>" rel="stylesheet" media="(max-height:0px)" onload="if(media!='all')media='all'"/>
+<link href="/css/style.css?v=<?=$version?>" rel="stylesheet" media="(max-height:0px)" onload="if(media!='all')media='all'"/>
+<?php endif; ?>
