6f50b95829a6b4c2f167ed6b335812500c1994dd
ShtHappens796 <shithappens796@gmail.com>
2017-11-26 20:18:11 +0200

    Move back to jQuery 3.2.1

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -1,3 +1,3 @@
-<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="/js/materialize.min.js?v=<?=$version?>"></script>
 <script src="/js/init.js?v=<?=$version?>"></script>
