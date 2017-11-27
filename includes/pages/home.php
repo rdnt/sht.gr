b110961c530698497a7f0e815c71ff407525832a
ShtHappens796 <shithappens796@gmail.com>
2017-11-27 03:02:25 +0200

    Parallax

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -1,1 +1,14 @@
-Home
+<div id="home-parallax" class="parallax-container valign-wrapper">
+    <div class="section container">
+        <div class="center-align">
+
+                <img src="images/home/logo.svg">
+            
+            <h5>Hey there.</h5>
+            <h3>I am Tasos.</h3>
+            <h6>Web Design, Application Development, 3d Graphics & Animation, Music Production</h6>
+            <div href="#main"><img class="arrow" src="/images/home/arrow_down.png"></div>
+        </div>
+    </div>
+    <div class="parallax"><img src="/images/home/parallax.jpg"></div>
+</div>
