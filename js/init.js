261c63c0d7921672b7f4ba6672e9ff7811e6b85e
ShtHappens796 <shithappens796@gmail.com>
2017-12-10 01:22:03 +0200

    init.js changes

--- js/init.js
+++ js/init.js
@@ -1,16 +1,12 @@
 (function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
+    $(function(){
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 // DOM content loaded
 $(function(){
-
-
 
 });
 
@@ -26,20 +22,18 @@ $(window).on('load', function(){
     setTimeout(function(){
         $("#right-panel").addClass("slide-in slide-right");
         $("#left-panel").addClass("slide-in slide-left");
-    }, 100);
-
-    setTimeout(function(){
         setTimeout(function(){
-            $("#right-panel").removeClass("init-right");
-            $("#left-panel").removeClass("init-left");
             setTimeout(function(){
-                $("#loader").addClass("display-none");
-            }, 1000);
-        }, 500);
-        $("#logo-wrapper").addClass("invisible");
-        setTimeout(function(){
-            $("#loader").addClass("transparent");
-        }, 50);
-    }, 1100);
-
+                $("#right-panel").removeClass("init-right");
+                $("#left-panel").removeClass("init-left");
+                setTimeout(function(){
+                    $("#loader").addClass("display-none");
+                }, 1000);
+            }, 500);
+            $("#logo-wrapper").addClass("invisible");
+            setTimeout(function(){
+                $("#loader").addClass("transparent");
+            }, 50);
+        }, 1000);
+    }, 100);
 });
