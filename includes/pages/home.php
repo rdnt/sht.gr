9435eb3d7dc82f186393e9ac53ef891dada37a8a
ShtHappens796 <shithappens796@gmail.com>
2017-12-10 11:14:36 +0200

    Add shadows

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -11,20 +11,22 @@
     <div  class="parallax"><img id="parallax-img" class="transparent" alt="" src="/images/home/parallax.jpg"></div>
 </div>
 <a id="main"></a>
-<div class="container home-section">
-    <div class="row valign-wrapper">
-        <div class="col l6 right-align valign">
-            <h4 class="light-text">Code</h4>
-            <h5 class="light-text">Whether it being a very specific project or a routine activity, programming plays a big part in a workplace.<br>Especially in mine.</h5>
-            <a>Coding Projects</a>
-        </div>
-        <div class="col l5 offset-l1 valign">
-            <div id="watch-container">
-                <img class="shadow" src="/images/home/shadow.svg">
-                <img class="case" src="/images/home/case.svg">
-                <img class="crown" src="/images/home/crown.svg">
-                <div class="hover-overlay"></div>
-                <img class="screen" src="/images/home/screen.svg">
+<div class="z-depth-3">
+    <div class="container home-section">
+        <div class="row valign-wrapper">
+            <div class="col l6 right-align valign">
+                <h4 class="light-text">Code</h4>
+                <h5 class="light-text">Whether it being a very specific project or a routine activity, programming plays a big part in a workplace.<br>Especially in mine.</h5>
+                <a>Coding Projects</a>
+            </div>
+            <div class="col l5 offset-l1 valign">
+                <div id="watch-container">
+                    <img class="shadow" src="/images/home/shadow.svg">
+                    <img class="case" src="/images/home/case.svg">
+                    <img class="crown" src="/images/home/crown.svg">
+                    <div class="hover-overlay"></div>
+                    <img class="screen" src="/images/home/screen.svg">
+                </div>
             </div>
         </div>
     </div>
@@ -42,15 +44,17 @@
     </div>
     <div  class="parallax"><img id="gaming-img" class="transparent" alt="" src="/images/home/gaming-bg.png"></div>
 </div>
-<div class="container home-section">
-    <div class="row valign-wrapper">
-        <div class="col l6 right-align valign">
-            <h4 class="light-text">Music</h4>
-            <h5 class="light-text">A straightforward way to relax and express oneself.<br>But it doesn't end there.<br>It never will.</h5>
-            <a>Music Catalog</a>
-        </div>
-        <div class="col l5 offset-l1 valign">
-            <div style="background-color:#ddddff;height:300px;width:300px;"></div>
+<div class="z-depth-2">
+    <div class="container home-section">
+        <div class="row valign-wrapper">
+            <div class="col l6 right-align valign">
+                <h4 class="light-text">Music</h4>
+                <h5 class="light-text">A straightforward way to relax and express oneself.<br>But it doesn't end there.<br>It never will.</h5>
+                <a>Music Catalog</a>
+            </div>
+            <div class="col l5 offset-l1 valign">
+                <div style="background-color:#ddddff;height:300px;width:300px;"></div>
+            </div>
         </div>
     </div>
 </div>
