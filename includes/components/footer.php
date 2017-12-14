dbc7419b78de67b4b4736fd9a360d0dc9c5a301b
ShtHappens796 <shithappens796@gmail.com>
2017-12-14 22:20:58 +0200

    Fix visual shadow glitch on footer

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -1,32 +1,34 @@
 <footer class="sht-black">
-    <div class="container footer-main z-depth-1">
-        <div class="row">
-            <div class="col l3 s12">
-                <h5>Source</h5>
-                <h6>The source code of my website is available here on <a href="https://github.com/ShtHappens796/sht.gr" target="_blank">GitHub</a>.</h6>
-            </div>
-            <div class="col l4 offset-l1 s12">
-                <h5>Social</h5>
-                <ul class="social-links">
-                    <li><a href="https://www.facebook.com/ShtHappens796" target="_blank"><img src="/images/social/facebook.svg"></a></li>
-                    <li><a href="https://www.instagram.com/shthappens796/" target="_blank"><img src="/images/social/instagram.svg"></a></li>
-                    <li><a href="https://www.linkedin.com/in/tasos-papalyras" target="_blank"><img src="/images/social/linkedin.svg"></a></li>
-                    <li><a href="https://github.com/ShtHappens796" target="_blank"><img src="/images/social/github.svg"></a></li>
-                    <li><a href="https://www.youtube.com/channel/UCh6P1iqAuWUk6wlv8KoxivA" target="_blank"><img src="/images/social/youtube.svg"></a></li>
-                    <li><a href="https://www.mixcloud.com/theofficialsht/" target="_blank"><img src="/images/social/mixcloud.svg"></a></li>
-                    <li><a href="https://soundcloud.com/theofficialsht" target="_blank"><img src="/images/social/soundcloud.svg"></a></li>
-                </ul>
-            </div>
-            <div class="col l3 offset-l1 s12 sitemap">
-                <h5>Sitemap</h5>
-                <ul class="left">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/projects.php">Projects</a></li>
-                </ul>
-                <ul class="right">
-                    <li><a href="/music.php">Music</a></li>
-                    <li><a href="/about.php">About</a></li>
-                </ul>
+    <div class="footer-main z-depth-2">
+        <div class="container">
+            <div class="row">
+                <div class="col l3 s12">
+                    <h5>Source</h5>
+                    <h6>The source code of my website is available here on <a href="https://github.com/ShtHappens796/sht.gr" target="_blank">GitHub</a>.</h6>
+                </div>
+                <div class="col l4 offset-l1 s12">
+                    <h5>Social</h5>
+                    <ul class="social-links">
+                        <li><a href="https://www.facebook.com/ShtHappens796" target="_blank"><img src="/images/social/facebook.svg"></a></li>
+                        <li><a href="https://www.instagram.com/shthappens796/" target="_blank"><img src="/images/social/instagram.svg"></a></li>
+                        <li><a href="https://www.linkedin.com/in/tasos-papalyras" target="_blank"><img src="/images/social/linkedin.svg"></a></li>
+                        <li><a href="https://github.com/ShtHappens796" target="_blank"><img src="/images/social/github.svg"></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCh6P1iqAuWUk6wlv8KoxivA" target="_blank"><img src="/images/social/youtube.svg"></a></li>
+                        <li><a href="https://www.mixcloud.com/theofficialsht/" target="_blank"><img src="/images/social/mixcloud.svg"></a></li>
+                        <li><a href="https://soundcloud.com/theofficialsht" target="_blank"><img src="/images/social/soundcloud.svg"></a></li>
+                    </ul>
+                </div>
+                <div class="col l3 offset-l1 s12 sitemap">
+                    <h5>Sitemap</h5>
+                    <ul class="left">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/projects.php">Projects</a></li>
+                    </ul>
+                    <ul class="right">
+                        <li><a href="/music.php">Music</a></li>
+                        <li><a href="/about.php">About</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
