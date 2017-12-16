f8093cd0e539aab6d485141aa732bce1c2411d26
ShtHappens796 <shithappens796@gmail.com>
2017-12-16 04:44:40 +0200

    Hover effects on footer and homepage

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col l3 s12">
                     <h5>Source</h5>
-                    <h6>The source code of my website is available here on <a href="https://github.com/ShtHappens796/sht.gr" target="_blank">GitHub</a>.</h6>
+                    <h6>The source code of my website is available on <a class="anchor white" href="https://github.com/ShtHappens796/sht.gr" target="_blank">GitHub</a>.</h6>
                 </div>
                 <div class="col l4 offset-l1 s12">
                     <h5>Social</h5>
@@ -21,12 +21,12 @@
                 <div class="col l3 offset-l1 s12 sitemap">
                     <h5>Sitemap</h5>
                     <ul class="left">
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/projects.php">Projects</a></li>
+                        <li><a class="anchor white" href="/">Home</a></li>
+                        <li><a class="anchor white" href="/projects.php">Projects</a></li>
                     </ul>
                     <ul class="right">
-                        <li><a href="/music.php">Music</a></li>
-                        <li><a href="/about.php">About</a></li>
+                        <li><a class="anchor white" href="/music.php">Music</a></li>
+                        <li><a class="anchor white" href="/about.php">About</a></li>
                     </ul>
                 </div>
             </div>
@@ -34,7 +34,7 @@
     </div>
     <div class="footer-copyright">
         <div class="container">
-            <span>© 2017 <a href="/">SHT</a> - All Rights Reserved</span>
+            <span>© 2017 <a class="anchor white" href="/">SHT</a> - All Rights Reserved</span>
         </div>
     </div>
 </footer>
