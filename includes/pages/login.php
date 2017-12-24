bf69f44f7d3a1db0256676adfdb072011c204376
ShtHappens796 <shithappens796@gmail.com>
2017-12-24 18:02:08 +0200

    Login page layout fixes

--- includes/pages/login.php
+++ includes/pages/login.php
@@ -3,9 +3,19 @@
     <div class="login-wrapper sht-depth-3">
         <h5>Login</h5>
         <form id="login_form" action="/backend/login.php" method="post">
-            <input type="text" placeholder="Username" name="username" required>
-            <input type="password" placeholder="Password" name="password" required>
-            <button class="btn blue" type="submit">Login</button>
+
+                <div class="input-field col s12">
+                    <input id="username" name="username" type="text" required autofocus>
+                    <label for="username">Username</label>
+                </div>
+
+                <div class="input-field col s12">
+                    <input id="password" name="password" type="password" required>
+                    <label for="password">Password</label>
+                </div>
+
+            <a class="anchor black" href="/">Go Back</a>
+            <button class="btn blue right" type="submit">Login</button>
         </form>
         <h6 class="cms">SHT CMS v<?=$sht->getVersion()?></h6>
     </div>
