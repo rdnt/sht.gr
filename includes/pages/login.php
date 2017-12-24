43982ff85b397544a9db6f850badb39d8885fd2c
ShtHappens796 <shithappens796@gmail.com>
2017-12-24 08:35:53 +0200

    Login layout

--- includes/pages/login.php
+++ includes/pages/login.php
@@ -1,15 +1,12 @@
-
-    <div id="login-container">
-        <div class="login-wrapper">
-        <form action="/backend/login.php" method="post">
-
+<div id="login-bg-container"></div>
+<div id="login-container">
+    <div class="login-wrapper sht-depth-3">
+        <h5>Login</h5>
+        <form id="login_form" action="/backend/login.php" method="post">
             <input type="text" placeholder="Username" name="username" required>
-
             <input type="password" placeholder="Password" name="password" required>
-
-            <button type="submit">Login</button>
-
-
+            <button class="btn blue" type="submit">Login</button>
         </form>
+        <h6 class="cms">SHT CMS v<?=$sht->getVersion()?></h6>
     </div>
 </div>
