25605060c1e2059494cb8a1c289ac4b2aa7ff403
ShtHappens796 <shithappens796@gmail.com>
2017-12-24 20:03:57 +0200

    Login animations

--- includes/pages/login.php
+++ includes/pages/login.php
@@ -1,22 +1,61 @@
 <div id="login-bg-container"></div>
 <div id="login-container">
     <div class="login-wrapper sht-depth-3">
-        <h5>Login</h5>
-        <form id="login_form" action="/backend/login.php" method="post">
+        <div class="default">
+            <h5>Login</h5>
+            <h6>Insert your credentials below</h6>
 
-                <div class="input-field col s12">
-                    <input id="username" name="username" type="text" required autofocus>
-                    <label for="username">Username</label>
-                </div>
+            <h6 class="cms">SHT CMS v<?=$sht->getVersion()?></h6>
+        </div>
+        <div class="containers">
+            <div class="login-content">
 
-                <div class="input-field col s12">
-                    <input id="password" name="password" type="password" required>
-                    <label for="password">Password</label>
-                </div>
+                <form id="login_form" action="/backend/login.php" method="post">
 
-            <a class="anchor black" href="/">Go Back</a>
-            <button class="btn blue right" type="submit">Login</button>
-        </form>
-        <h6 class="cms">SHT CMS v<?=$sht->getVersion()?></h6>
+                        <div class="input-field col s12">
+                            <input id="username" name="username" type="text" required autofocus>
+                            <label for="username">Username</label>
+                        </div>
+
+                        <div class="input-field col s12">
+                            <input id="password" name="password" type="password" required>
+                            <label for="password">Password</label>
+                        </div>
+
+
+                    <button class="btn blue right" type="submit">Login</button>
+                </form>
+
+            </div>
+            <div class="code-content">
+                <form id="code_form" action="/backend/2fa-login.php" method="post">
+
+
+
+                        <div class="input-field col s12">
+                            <input id="code" name="code" type="text" required>
+                            <label for="code">Authentication Code</label>
+                        </div>
+
+
+                    <button class="btn blue right" type="submit">Verify</button>
+                </form>
+
+            </div>
+
+            <div class="fingerprint-content">
+                <form id="fingerprint_form" action="/backend/2fa-login.php" method="post">
+
+
+                    <i class="material-icons">fingerprint</i>
+                    <h6>Awaiting fingerprint</h6>
+
+
+
+                </form>
+
+            </div>
+
+        </div>
     </div>
 </div>
