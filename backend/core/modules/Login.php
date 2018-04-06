778450e2aa739c1a6d3e1425c76e8197f9db1eee
Tasos Papalyras <shithappens796@gmail.com>
2018-04-06 17:26:50 +0300

    Fix bug on logging in (root datamember access)

--- backend/core/modules/Login.php
+++ backend/core/modules/Login.php
@@ -3,13 +3,13 @@
 trait Login {
     // Check if the account exists
     function checkAccountExistence($username) {
-        if (!file_exists($this->root . "/data/accounts/$username.json")) {
+        if (!file_exists($this->getRoot() . "/data/accounts/$username.json")) {
             $this->response("ACCOUNT_DOES_NOT_EXIST");
         }
     }
     // Check password with the one saved
     function checkPassword($username, $password) {
-        $user = file_get_contents($this->root . "/data/accounts/$username.json");
+        $user = file_get_contents($this->getRoot() . "/data/accounts/$username.json");
         $userdata = json_decode($user, true);
         // Check if password is correct
         if (!password_verify($password, $userdata['password-hash'])) {
