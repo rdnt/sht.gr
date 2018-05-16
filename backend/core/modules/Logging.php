2964bcce9d50d2bba3742401a18dedf61f88078d
ShtHappens796 <shithappens796@gmail.com>
2018-05-16 23:54:42 +0300

    Add logging module

---
+++ backend/core/modules/Logging.php
@@ -0,0 +1,41 @@
+<?php
+// Trait that handles logging
+trait Logging {
+    // Logs an action
+    function log($action, $data, $origin = null) {
+        // Get the latest.log path
+        $logs_folder = $this->getRoot() . "/data/logs/";
+        $log = $logs_folder . "latest.log";
+        // Add date header
+        $date = date("d M Y H:i:s");
+        // Append the origin
+        if ($origin) {
+            $origin = " from " . $origin;
+        }
+        else {
+            $origin = null;
+        }
+        // Create the message
+        $message = $date . " $action: $data$origin\n";
+        if (file_exists($log)) {
+            // Latest log exists; load it
+            $data = file_get_contents($log);
+            $lines = COUNT(FILE($log));
+            if ($lines >= 1000) {
+                // Dump log and create a new one
+                $date = date("Ymd_His");
+                file_put_contents("$logs_folder$date.log", $data);
+                $data = $message;
+            }
+            else {
+                // Just append the message to existin log
+                $data .= $message;
+            }
+        }
+        else {
+            $data = $message;
+        }
+        // Save the latest log
+        file_put_contents($log, $data);
+    }
+}
