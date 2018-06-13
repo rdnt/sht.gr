0623c7440c43e4f93e111bf9c1d258d3f31dc715
ShtHappens796 <shithappens796@gmail.com>
2018-06-14 00:35:08 +0300

    Fix folder rendering

---
+++ js/core.js
@@ -0,0 +1,51 @@
+var ajaxRequestPending = 0;
+function asyncFormSubmission(form_id, target_url = null, init, callback, min_delay = 0, error_delay = 0, rapid_error = false) {
+    $(form_id).submit(function(e) {
+        e.preventDefault();
+        if (!ajaxRequestPending) {
+            var url = "";
+            if (target_url instanceof Function) {
+                url = target_url();
+            }
+            else {
+                url = target_url;
+            }
+            init();
+            ajaxRequestPending = 1;
+            var start = new Date();
+            $.ajax({
+                method: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function(data) {
+                    data = $.trim(data);
+                    var duration = new Date() - start;
+                    try {
+                        data = JSON.parse(data);
+                    }
+                    catch(e) {
+                        data = {"response": "JSON_PARSE_FAILED"};
+                    }
+                    if (data['response'] === "SUCCESS" || rapid_error === false) {
+                        setTimeout(function(){
+                            ajaxRequestPending = 0;
+                            callback(data);
+                        }, min_delay - duration);
+                    }
+                    else if (rapid_error === true) {
+                        callback(data);
+                        setTimeout(function(){
+                            ajaxRequestPending = 0;
+                        }, error_delay);
+                    }
+                    else {
+                        setTimeout(function(){
+                            callback(data);
+                            ajaxRequestPending = 0;
+                        }, error_delay);
+                    }
+                }
+            });
+        }
+    });
+}
