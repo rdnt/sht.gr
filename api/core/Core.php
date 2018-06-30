9edc37747a2a5cd6f15b95dc37ef4de5a8937847
ShtHappens796 <shithappens796@gmail.com>
2018-06-30 21:24:04 +0300

    Fix bug with explode index selection

--- api/core/Core.php
+++ api/core/Core.php
@@ -141,7 +141,7 @@ abstract class Core {
     // Returns the blueprint selected for a page
     function getBlueprint($page) {
         $parameters = explode("/", $page);
-        $page = $parameters[0];
+        $page = $parameters[1];
         if (array_key_exists($page, $this->pages)){
             return $this->pages[$page][2];
         }
@@ -158,7 +158,7 @@ abstract class Core {
     // Renders a page depending on a blueprint
     function renderPage() {
         $parameters = explode("/", $this->getCurrentPage());
-        $folder = $parameters[0];
+        $folder = $parameters[1];
         if (!in_array($folder, $this->folders) && $folder !== "api") {
             $name = $this->getCurrentPage();
             require_once $this->getBlueprintPath();
