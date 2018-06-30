c6041f9e1f03bb7a9e76d4547d79ebfd75d0c040
ShtHappens796 <shithappens796@gmail.com>
2018-06-30 18:03:18 +0300

    Fix rendering bug due to explode(), add head and scripts contents

--- includes/components/scripts.php
+++ includes/components/scripts.php
@@ -1,3 +1,6 @@
 <script src="/js/jquery.min.js?v=<?=$this->getLatestCommit()?>"></script>
 <script src="/js/async.js?v=<?=$this->getLatestCommit()?>"></script>
+<<<<<<< HEAD
 <script src="/js/init.js?v=<?=$this->getLatestCommit()?>"></script>
+=======
+>>>>>>> e2b982e... Fix rendering bug due to explode(), add head and scripts contents
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 <?php require_once getcwd()."/api/core/Core.php";
+=======
+<?php require_once $_SERVER['DOCUMENT_ROOT']."/api/core/Core.php";
+>>>>>>> e2b982e... Fix rendering bug due to explode(), add head and scripts contents
