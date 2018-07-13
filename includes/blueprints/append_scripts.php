beffda9b4001d65f148d70373568139ce7f0722b
ShtHappens796 <shithappens796@gmail.com>
2018-07-13 23:52:07 +0300

    Re-factor blueprint, page rendering & rewriting and component loading to allow the placement of the Core in subfolders.

--- includes/blueprints/append_scripts.php
+++ includes/blueprints/append_scripts.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php require_once $_SERVER['DOCUMENT_ROOT']."/includes/components/head.php"; ?>
+<?php $this->loadComponent("head"); ?>
 <title><?=$this->getPageTitle(); ?></title>
 </head>
 <body>
-<?php require_once $_SERVER['DOCUMENT_ROOT']."/includes/components/nav.php"; ?>
+<?php $this->loadComponent("nav"); ?>
 <main>
 <?php $this->getPageSegment("<!-- SCRIPTS -->", 0)?>
 </main>
-<?php require_once $_SERVER['DOCUMENT_ROOT']."/includes/components/footer.php"; ?>
-<?php require_once $_SERVER['DOCUMENT_ROOT']."/includes/components/scripts.php"; ?>
+<?php $this->loadComponent("footer"); ?>
+<?php $this->loadComponent("scripts"); ?>
 <?php $this->getPageSegment("<!-- SCRIPTS -->", 1); ?>
 </body>
 </html>
