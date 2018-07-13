abee1fb79334bacdde2f9272a1f6f58ce181028c
ShtHappens796 <shithappens796@gmail.com>
2018-07-14 00:04:45 +0300

    Fix static resource URL resolving

--- includes/components/scripts.php
+++ includes/components/scripts.php
@@ -1,2 +1,2 @@
-<script src="/js/jquery.min.js?v=<?=$this->getLatestCommit()?>"></script>
-<script src="/js/async.js?v=<?=$this->getLatestCommit()?>"></script>
+<script src="js/jquery.min.js?v=<?=$this->getLatestCommit()?>"></script>
+<script src="js/async.js?v=<?=$this->getLatestCommit()?>"></script>
