fec8d26fa04d825f3f4895da67513c2e0530b4f9
ShtHappens796 <shithappens796@gmail.com>
2018-07-14 00:04:45 +0300

    Fix static resource URL resolving

--- includes/components/head.php
+++ includes/components/head.php
@@ -3,9 +3,5 @@
 <meta name="description" content="">
 <meta name="keywords" content="">
 <meta name="author" content="SHT">
-<<<<<<< HEAD
 <link href="/css/sht.css?v=<?=$this->getLatestCommit()?>" type="text/css" rel="stylesheet" media="screen"/>
 <link href="/css/responsive.css?v=<?=$this->getLatestCommit()?>" type="text/css" rel="stylesheet" media="screen"/>
-=======
-<link href="/css/core.css?v=<?=$this->getLatestCommit()?>" type="text/css" rel="stylesheet" media="screen"/>
->>>>>>> e2b982e... Fix rendering bug due to explode(), add head and scripts contents
--- includes/components/scripts.php
+++ includes/components/scripts.php
@@ -1,6 +1,3 @@
 <script src="/js/jquery.min.js?v=<?=$this->getLatestCommit()?>"></script>
 <script src="/js/async.js?v=<?=$this->getLatestCommit()?>"></script>
-<<<<<<< HEAD
 <script src="/js/init.js?v=<?=$this->getLatestCommit()?>"></script>
-=======
->>>>>>> e2b982e... Fix rendering bug due to explode(), add head and scripts contents
