11e8460717daa4e17d6b4e177f0105a9a4853195
ShtHappens796 <shithappens796@gmail.com>
2018-09-29 00:05:34 +0300

    Start from the Core and build from the ground up

--- backend/core/sht-cms.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once $_SERVER['DOCUMENT_ROOT']."/backend/core/core.php";
-include_once $_SERVER['DOCUMENT_ROOT']."/backend/core/post.php";
-
-$sht = new SHT_CMS;
-
-?>
--- includes/components/scripts.php
+++
@@ -1,3 +0,0 @@
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script src="/js/materialize.min.js?v=<?=$sht->getVersion()?>"></script>
-<script src="/js/init.js?v=<?=$sht->getVersion()?>"></script>
