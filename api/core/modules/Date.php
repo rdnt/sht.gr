d9f3a420d38192542410b178c8a7548bee27cea0
ShtHappens796 <shithappens796@gmail.com>
2018-09-28 23:57:26 +0300

    Add date module into Core, add documentation on the Database class

---
+++ api/core/modules/Date.php
@@ -0,0 +1,15 @@
+<?php
+// Trait that handles date formatting
+trait Date {
+    /**
+     * Extracts the format of the date supplied, formats and returns it
+     *
+     * @param string $date The date to parse
+     * @param string $format The format in which the date will be formatted to
+     * @return string The formatted date string
+     */
+    function date($date, $format) {
+        $date = strtotime($date);
+        return date($format, $date);
+    }
+}
