e7c5a397b3dfb83de10ebea4a1ab0b212b7dd675
ShtHappens796 <tasos@sht.gr>
2018-10-25 15:52:05 +0300

    bump patch version

--- api/core/Core.php
+++ api/core/Core.php
@@ -12,7 +12,7 @@
  * @author    Tasos Papalyras <tasos@sht.gr>
  * @copyright 2018 Tasos Papalyras
  * @license   https://github.com/ShtHappens796/Core/blob/master/LICENSE MIT
- * @version   2.0.0 (25 October 2018)
+ * @version   2.0.5 (25 October 2018)
  * @link      https://github.com/ShtHappens796/Core
  *
  */
