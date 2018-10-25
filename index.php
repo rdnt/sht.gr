e7185787df91b0130d7e35ec7af20ac6435722d2
ShtHappens796 <tasos@sht.gr>
2018-10-25 15:40:30 +0300

    Remove debugging

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once getcwd()."/api/core/Core.php";
+<?php require_once getcwd()."/api/core/Core.php";
