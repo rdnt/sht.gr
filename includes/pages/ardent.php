b45c0dbc4c6b27c468cdff5a02762ebe77440a21
ShtHappens796 <tasos@sht.gr>
2018-11-02 01:34:28 +0200

    Commit changes for now

--- includes/pages/ardent.php
+++ includes/pages/ardent.php
@@ -1,10 +1,13 @@
 <div class="player unselectable">
     <div class="ring">
         <div class="center">
-            <img src="/images/play.svg">
+            <img class="current" src="/images/play.svg">
+            <img class="current" src="/images/play.svg">
         </div>
         <div class="background">
         </div>
     </div>
+    <div class="progress">
 
+    </div>
 </div>
