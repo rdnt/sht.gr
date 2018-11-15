c02e6415a5b70bc40358d3b5b567bf372beccf4f
ShtHappens796 <tasos@sht.gr>
2018-11-15 23:45:59 +0200

    Controls

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -1,4 +1,15 @@
 <div class="player">
     <div class="progress"></div>
     <div class="overlay"></div>
+    <div class="controls">
+        <div class="control left">
+
+        </div>
+        <div class="control center play">
+
+        </div>
+        <div class="control right">
+
+        </div>
+    </div>
 </div>
