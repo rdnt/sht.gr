e9ce06cd9b547aa29a3c46624755916997900b30
ShtHappens796 <tasos@sht.gr>
2018-11-16 02:46:57 +0200

    new nav

--- includes/components/footer.php
+++ includes/components/footer.php
@@ -1,3 +1,23 @@
+<nav>
+    <div class="header">
+        <span>Back</span>
+    </div>
+    <ul class="navigation">
+        <li class="active">
+            <span>Ardent Radio</span>
+        </li>
+        <li>
+            <span>Original Mixes</span>
+        </li>
+        <li>
+            <span>Remixes</span>
+        </li>
+        <li>
+            <span>Mashups</span>
+        </li>
+    </ul>
+</nav>
+
 <div class="player">
     <div class="progress"></div>
     <div class="overlay"></div>
