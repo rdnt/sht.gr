62ed7fce872281666d9fb6d75e37c618bcc87305
ShtHappens796 <tasos@sht.gr>
2018-11-19 02:46:11 +0200

    Cleanup documentation on database module

--- api/core/modules/Database.php
+++ api/core/modules/Database.php
@@ -3,72 +3,53 @@
 // Trait that handles database connections and performing querries
 class Database extends mysqli {
 
-    protected $shell;
-    private $host;
-    private $user;
-    private $db;
-    private $pass;
-
     /**
-     * Connects to the database using a persistent socket if the connection
-     * is not already active
+     * Connects to the database server using a persistent socket if the
+     * connection is not already active. If a database is specified it connects
+     * To that specific database.
      *
-     * @param string $shell The shell object
+     * @param object $shell The shell object
      * @param string $host The hostname to which to connect to
      * @param string $user Database username
-     * @param string $db Reference to the database connection
+     * @param object $db Reference to the database connection
      */
     function __construct($shell, $host, $user, $db = null) {
-
-        $this->shell = $shell;
-
-        $this->host = $host;
-        $this->user = $user;
-        $this->db = $db;
-
-        $db_pass = dirname($_SERVER['DOCUMENT_ROOT']) . "/db.pass";
-        if (!file_exists($db_pass)) {
-            $this->shell->log("DATABASE", "Missing password file");
-            $this->shell->setCurrentPage("/error/503");
-            return;
+        // Calculate the database password path
+        $pass_path = dirname($_SERVER['DOCUMENT_ROOT']) . "/db.pass";
+        if (!file_exists($pass_path)) {
+            // No db password file? Throw a 503 error and log it
+            $shell->log("DATABASE", "Missing password file");
+            $shell->setCurrentPage("/error/503");
+            return false;
         }
-        $this->pass = file_get_contents($db_pass);
-
-        $this->connect($this->host, $this->user, $this->pass, $this->db);
-    }
-
-    function connect($host = NULL, $user = NULL, $password = NULL, $database = NULL, $port = NULL, $socket = NULL) {
-
+        // Get the db server password
+        $pass = file_get_contents($pass_path);
         // Connect to the database server while suppressing warnings
-        if ($this->db) {
-            @parent::__construct("p:" . $this->host, $this->user, $this->pass, $this->db);
+        if ($db) {
+            @parent::__construct("p:" . $host, $user, $pass, $db);
         }
         else {
-            @parent::__construct("p:" . $this->host, $this->user, $this->pass);
+            @parent::__construct("p:" . $host, $user, $pass);
         }
-        $this->shell->log("DATABASE", "All Good");
         if (mysqli_connect_error()) {
-            // Log the error
-            $this->shell->log("DATABASE", mysqli_connect_error());
-            $this->shell->setCurrentPage("/error/503");
+            // Log the errors if any
+            $shell->log("DATABASE", mysqli_connect_error());
+            // Error during connecting to the database? Throw a 503 error
+            $shell->setCurrentPage("/error/503");
+            return false;
         }
     }
-
-    function recreateDB($db) {
-        $this->close();
-        $this->connect($this->host, $this->user, $this->pass);
-        $this->query("
-            DROP DATABASE $db;
-        ");
-        $this->query("
-            CREATE DATABASE $db;
-        ");
-        $this->close();
-        $this->connect($this->host, $this->user, $this->pass, $db);
-    }
-
+    
+    /**
+     * Performs a multi-query against the database and returns the status
+     *
+     * @param string $sql The sequence of SQL queries to perform
+     * @return boolean The status of the query.
+     */
     function multi_query($sql) {
         $status = parent::multi_query($sql);
+        // While there are results iterate and ignore them so that subsequent
+        // queries work
         while ($this->next_result()) {
             if (!$this->more_results()) {
                 break;
@@ -80,26 +61,32 @@ class Database extends mysqli {
     /**
      * Performs a query against the database and returns the result
      *
-     * @param string $sql The sql query
+     * @param string $sql The SQL query to perform
+     * @return mixed The result of the query:
+     *               If the rows are zero, it returns false. Else, it returns an
+     *               associative array containing data for each row as follows:
+     *                 Based on the number of columns:
+     *                  - 1: The value of the column
+     *                  - 2: A key-value pair of the first column with the 2nd
+     *                  - 3 or more: First row is treated as a key pointing to
+     *                               an associative array containing the rest
+     *                               of the columns.
      */
     function query($sql, $resultmode = NULL) {
         $response = parent::query($sql, $resultmode);
-        return $this->processResponse($response);
-    }
-
-    function processResponse($response) {
         if (!is_bool($response)) {
             if (mysqli_num_rows($response) != 0) {
                 $data = array();
                 // return all rows
                 while($row = $response->fetch_assoc()) {
-                    if (sizeof($row) > 2) {
+                    $columns = sizeof($row);
+                    if ($columns > 2) {
                         $data[array_shift($row)] = $row;
                     }
-                    else if (sizeof($row) == 2) {
+                    else if ($columns == 2) {
                         $data[array_shift($row)] = reset($row);
                     }
-                    else if (sizeof($row) == 1) {
+                    else if ($columns == 1) {
                         $data = reset($row);
                     }
                 }
@@ -113,11 +100,11 @@ class Database extends mysqli {
     }
 
     /**
-     * Parses the password that helps encrypt userdata inside the database
+     * Parses the key that helps encrypt userdata while at rest the database
      *
-     * @return string The password for the specified database user
+     * @return string The encryption key
      */
-    function getSecretKey() {
+    function getEncryptionKey() {
         return file_get_contents(dirname($_SERVER['DOCUMENT_ROOT']) . "/db.key");
     }
 
