ee5d72c8cbfd16e10a83a7f153120330a591edb5
ShtHappens796 <tasos@sht.gr>
2018-11-25 01:57:36 +0200

    Disable transitions during theming for smoother theme change

--- js/init.js
+++ js/init.js
@@ -1,12 +1,17 @@
 
 $(window).on("load", function() {
 	setTimeout(function() {
-		//$(".theme-overlay").addClass("dark");
-	},1000);
+		$(".theme-overlay").addClass("dark");
+		$("nav, main").addClass("theming");
+		setTimeout(function() {
+			$("nav, main").addClass("dark");
+			setTimeout(function() {
+				$("nav, main").removeClass("theming");
+			}, 500);
+		}, 900);
 
-	setTimeout(function() {
-		//$("nav, main").addClass("dark");
-	},2250);
+	}, 1000);
+
 
 	$("nav").hover(
 		function() {
@@ -18,7 +23,6 @@ $(window).on("load", function() {
 		function() {
 			if ($("nav").hasClass("load")) {
 				$("nav").removeClass("hover load");
-				console.log("removed");
 			}
 			else {
 				$("nav").removeClass("hover");
