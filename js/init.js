505852300b7c2b173e6cd0b5af4ce3d1bb7cfa03
ShtHappens796 <tasos@sht.gr>
2018-11-26 17:24:27 +0200

    Throw error instead

--- js/init.js
+++ js/init.js
@@ -1,11 +1,11 @@
 function callback(data) {
-    Console.log(data);
+
 }
 $(window).on("load", function() {
     if (asyncRequest instanceof Function) {
         asyncRequest("#form", "/api/endpoint", null, callback);
     }
     else {
-        Console.log("Function asyncRequest() not loaded.");
+        throw new Error("Function asyncRequest() not loaded.");
     }
 });
