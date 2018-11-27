9295887a77633e52f66247799304532282bfaf23
ShtHappens796 <tasos@sht.gr>
2018-11-27 16:15:05 +0200

    Remove recursive params testing

--- api/endpoint.php
+++ api/endpoint.php
@@ -1,16 +1,3 @@
 <?php
 // Example response
-$params = [
-    "outer_variable" => [
-        "inner_variable" => [
-            "inner_string_variable",
-            "inner_string_variable_with_regex" => "password"
-        ],
-        "outer_string_variable",
-        "outer_string_variable_with_regex" => "password"
-    ],
-    "string_variable",
-    "string_variable_with_regex" => "password"
-];
-$post = $core->validate($params);
 $core->response("SUCCESS");
