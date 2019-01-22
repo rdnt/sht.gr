26b79b70e7ce82498b18ce033cce6470b8bbc864
ShtHappens796 <tasos@sht.gr>
2019-01-22 18:47:31 +0200

    update logs trait

--- api/core/modules/Logging.php
+++ api/core/modules/Logging.php
@@ -3,48 +3,48 @@
 // Trait that handles logging
 trait Logging {
 
+    protected $logging;
+
     /**
      * Logs an action
      *
      * @param string $action The action to log
      * @param string $data The action data to log
-     * @param string $origin The origin client IP of the action
      */
-    function log($action, $data, $origin = null) {
-        // Get the latest.log path
-        $logs_folder = $this->getRoot() . "/logs/";
-        $log = $logs_folder . "latest.log";
-        // Add date header
-        $date = date("d M Y H:i:s");
-        // Append the origin
-        if ($origin) {
-            $origin = " from " . $origin;
-        }
-        else {
-            $origin = null;
-        }
-        // Create the message
-        $message = $date . " $action: $data$origin\n";
-        if (file_exists($log)) {
-            // Latest log exists; load it
-            $data = file_get_contents($log);
-            $lines = COUNT(FILE($log));
-            if ($lines >= 1000) {
-                // Dump log and create a new one
-                $date = date("Ymd_His");
-                file_put_contents("$logs_folder$date.log", $data);
-                $data = $message;
+    function log($action, $data) {
+        if ($this->options['logging']) {
+            // Get the latest.log path
+            $logs_folder = $this->getRoot() . "/logs/";
+            $log = $logs_folder . "latest.log";
+            // Add date header
+            $date = date("d M Y H:i:s");
+            // Create the message
+            $message = $date . " $action: $data\n";
+            if (file_exists($log)) {
+                // Latest log exists; load it
+                $data = file_get_contents($log);
+                $lines = COUNT(FILE($log));
+                if ($lines >= 1000) {
+                    // Dump log and create a new one
+                    $date = date("Ymd_His");
+                    file_put_contents("$logs_folder$date.log", $data);
+                    $data = $message;
+                }
+                else {
+                    // Just append the message to existin log
+                    $data .= $message;
+                }
             }
             else {
-                // Just append the message to existin log
-                $data .= $message;
+                $data = $message;
             }
+            // Save the latest log
+            file_put_contents($log, $data);
+            return true;
         }
         else {
-            $data = $message;
+            return false;
         }
-        // Save the latest log
-        file_put_contents($log, $data);
     }
 
     /**
