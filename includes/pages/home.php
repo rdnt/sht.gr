1bcdc5705aed4e7c47a1dd512877935f900c1395
ShtHappens796 <tasos@sht.gr>
2019-01-24 00:21:28 +0200

    Portfolio cards UI

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -1,6 +1,62 @@
 <div class="slider">
-    <div class="controls">
-        <div class="next"></div>
-        <div class="prev"></div>
+    <div class="content">
+        <div class="cards-container">
+            <div class="card">
+                <div class="inner">
+                    <span class="title">Shor10</span>
+                    <span class="description">Lorem ipsum si dolor tamet</span>
+                    <button class="btn">VIEW MORE</button>
+                </div>
+                <img class="bg" src="https://cdn.dribbble.com/users/13307/screenshots/5249168/800.jpg">
+            </div>
+            <div class="card">
+                <div class="inner">
+                    <span class="title">Test</span>
+                    <span class="description">Example description very long indeed lorem ipsum dolor sit amet wtf dude</span>
+                    <button class="btn">VIEW MORE</button>
+                </div>
+                <img class="bg" src="https://cdn.dribbble.com/users/2241163/screenshots/5634768/shor10_1x.png">
+            </div>
+            <div class="card">
+                <div class="inner">
+                    <span class="title">Shor10</span>
+                    <span class="description">Lorem ipsum si dolor tamet</span>
+                    <button class="btn">VIEW MORE</button>
+                </div>
+                <img class="bg" src="https://cdn.dribbble.com/users/581199/screenshots/4904405/xavier-designer-profiles-part-4.png">
+            </div>
+            <div class="card">
+                <img class="bg" src="https://cdn.dribbble.com/users/1493956/screenshots/5334381/artboard_2x_2x.png">
+            </div>
+            <div class="card">
+                <img class="bg" src="https://cdn.dribbble.com/users/2241163/screenshots/5634768/shor10_1x.png">
+            </div>
+            <div class="card">
+                <img class="bg" src="https://cdn.dribbble.com/users/13307/screenshots/5249168/800.jpg">
+            </div>
+            <div class="card">
+                <img class="bg" src="https://cdn.dribbble.com/users/581199/screenshots/4904405/xavier-designer-profiles-part-4.png">
+            </div>
+            <div class="card">
+                <img class="bg" src="https://cdn.dribbble.com/users/1493956/screenshots/5334381/artboard_2x_2x.png">
+            </div>
+            <div class="card">
+                <img class="bg" src="https://cdn.dribbble.com/users/2241163/screenshots/5634768/shor10_1x.png">
+            </div>
+            <div class="card">
+                <img class="bg" src="https://cdn.dribbble.com/users/13307/screenshots/5249168/800.jpg">
+            </div>
+            <div class="card">
+                <img class="bg" src="https://cdn.dribbble.com/users/581199/screenshots/4904405/xavier-designer-profiles-part-4.png">
+            </div>
+            <div class="card">
+                <img class="bg" src="https://cdn.dribbble.com/users/1493956/screenshots/5334381/artboard_2x_2x.png">
+            </div>
+        </div>
     </div>
+
+</div>
+<div class="controls">
+    <div class="next"></div>
+    <div class="prev"></div>
 </div>
--- js/init.js
+++ js/init.js
@@ -11,37 +11,37 @@ function toggleTheme() {
 	// 	}, 900);
 	// }, 1000);
 }
-
-$(window).on("load", function() {
-
-	$("nav").hover(
-		function() {
-			$(this).addClass("hover");
-		}
-	);
-
-	$("main").mousemove(
-		function() {
-			if ($("nav").hasClass("load")) {
-				$("nav").removeClass("hover load");
-			}
-			else {
-				$("nav").removeClass("hover");
-			}
-		}
-	);
-
-	$("main").hover(function() {
-		$("#side-reveal").addClass("hover");
-		$("#background-img").addClass("hover");
-	}, function() {
-		$("#side-reveal").removeClass("hover");
-		$("#background-img").removeClass("hover");
-	});
-
-	$("main").click(function() {
-		$("#side-reveal").addClass("active");
-		$("#background-img").addClass("active");
-	});
-
-});
+// 
+// $(window).on("load", function() {
+//
+// 	$("nav").hover(
+// 		function() {
+// 			$(this).addClass("hover");
+// 		}
+// 	);
+//
+// 	$("main").mousemove(
+// 		function() {
+// 			if ($("nav").hasClass("load")) {
+// 				$("nav").removeClass("hover load");
+// 			}
+// 			else {
+// 				$("nav").removeClass("hover");
+// 			}
+// 		}
+// 	);
+//
+// 	$("main").hover(function() {
+// 		$("#side-reveal").addClass("hover");
+// 		$("#background-img").addClass("hover");
+// 	}, function() {
+// 		$("#side-reveal").removeClass("hover");
+// 		$("#background-img").removeClass("hover");
+// 	});
+//
+// 	$("main").click(function() {
+// 		$("#side-reveal").addClass("active");
+// 		$("#background-img").addClass("active");
+// 	});
+//
+// });
