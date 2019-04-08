2f0d4b495d537ef42b93c4330398af1a2b35fa72
SHT <tasos@sht.gr>
2019-04-08 05:22:42 +0300

    Add frontend and backend texts (wip), code editors frontend

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -4,7 +4,7 @@
     </div>
     <div class="introduction">
         <span>
-            Hi, I am Tasos (a.k.a. SHT) and I am a web developer based in Corfu, Greece.
+            My name is Tasos, and I am a Full-Stack Web Developer based in Corfu, Greece.
             <br>
             <br>
             I create websites and interactive web applications from the ground up using leading technologies in the field such as PHP, MySQL and Javascript. From mockups to web design, development and testing, I make sure that an application is built with optimization and performance in mind, is deployed on a secure and robust environment and is friendly and appealing to the end-user, whether that is a systems' administrator or just a visitor.
@@ -59,14 +59,14 @@
         <div class="indicator"></div>
     </div>
 </div>
-<div id="prototyping" class="section">
+<div id="frontend" class="section">
     <div class="container">
         <div class="title">
-            <span>Prototyping</span>
+            <span>Frontend</span>
         </div>
-        <div class="info">
+        <div class="description">
             <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus maximus nisi ac imperdiet. Curabitur condimentum luctus tincidunt. Suspendisse potenti. Fusce sed iaculis ex, id varius massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras eget tempor nulla, sit amet molestie erat. Aliquam eu luctus tortor, non interdum odio. In id risus et lorem finibus tincidunt ut non quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas eu massa odio. Morbi ornare ante non elementum ultrices.
+                While creating a web application, the frontend will be the first thing the end-user will see once they arrive. A web app should be engaging, have interesting content and apparent structure, or else it will be difficult to navigate. I consider creating a highly efficient and maintainable user interface a big priority, therefore I ensure that the development environment has traits such as continous integration and compartmentalization. If any issue comes up, it will affect only a small part of the codebase and will be easily traceable and fixable.
             </span>
         </div>
         <div class="layers-container">
@@ -88,8 +88,75 @@
         </div>
     </div>
 </div>
-<div id="" class="section">
+<div id="backend" class="section">
     <div class="container">
+        <div class="title">
+            <span>Backend</span>
+        </div>
+        <div class="code-container">
+            <div class="editor">
+                <div class="header">
+                    <div class="control"></div>
+                    <div class="control"></div>
+                    <div class="control"></div>
+                </div>
+                <div class="content">
+                    <?php for($i=0; $i<40; $i++): ?>
+                        <div class="line">
+                            <?php for($j=0; $j<rand(0,2); $j++): ?>
+                                <div class="ind"></div>
+                                <div class="ind"></div>
+                                <div class="ind"></div>
+                                <div class="ind"></div>
+                            <?php endfor; ?>
+                            <?php for($j=0; $j<rand(4,20); $j++): ?>
+                                <div class="char"></div>
+                            <?php endfor; ?>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+                <div class="footer">
+                    <div class="status"></div>
+                </div>
+            </div>
+            <div class="editor">
+                <div class="header">
+                    <div class="control"></div>
+                    <div class="control"></div>
+                    <div class="control"></div>
+                </div>
+                <div class="content">
+                    <?php for($i=0; $i<40; $i++): ?>
+                        <div class="line">
+                            <?php for($j=0; $j<rand(0,2); $j++): ?>
+                                <div class="ind"></div>
+                                <div class="ind"></div>
+                                <div class="ind"></div>
+                                <div class="ind"></div>
+                            <?php endfor; ?>
+                            <?php for($j=0; $j<rand(8,20); $j++): ?>
+                                <div class="char"></div>
+                            <?php endfor; ?>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+                <div class="footer">
+                    <div class="status"></div>
+                </div>
+            </div>
+        </div>
+        <div class="description">
+            <span>
+                Implementing a usable frontend goes wasted without having a powerful backend to support it. I have experience working with modern content management systems such as Wordpress and Grav, and I have also developed a specialized framework in PHP called <a href="#" class="link dark">Core</a>, which enables me to create a fully working web application (like this one!) in hours.
+            </span>
+        </div>
 
     </div>
 </div>
+<!-- <div id="aboutme" class="section">
+    <div class="container">
+        <div class="title">
+            <span>About Me</span>
+        </div>
+    </div>
+</div> -->
